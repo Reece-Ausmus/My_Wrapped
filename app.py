412df4c969ccd7d48502ff676c66f0aea0f9ddae
Reece Ausmus <reece@ausmusfamily.com>
2024-12-27 15:46:41 -0500

    refactor to include wsgi

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
+import os
 import webbrowser
 from flask import Flask
 from auth import auth_bp
 from playback import playback_bp
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-# Load environment variables
-load_dotenv(override=True)
+db = SQLAlchemy()
+migrate = Migrate()
 
-app = Flask(__name__)
+def create_app():
+    # Load environment variables
+    load_dotenv(override=True)
 
-app.register_blueprint(auth_bp, url_prefix='/auth')
-app.register_blueprint(playback_bp, url_prefix='/playback')
+    app = Flask(__name__)
+
+     # Configure PostgreSQL Database
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(playback_bp, url_prefix='/playback')
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
+
 
 if __name__ == "__main__":
+    app = create_app()
     print("Starting the server... Opening the login page.")
     webbrowser.open('http://127.0.0.1:12398/auth')
     app.run(port=12398)

4e187c9ef87ba1e3c105f7fc08735cde158e02ea
Reece Ausmus <reece@ausmusfamily.com>
2024-12-27 17:32:32 -0500

    refactor to use html templates

--- playback.py
+++ playback.py
@@ -1,23 +1,17 @@
 from datetime import datetime
 from pytz import timezone
 import requests
-from flask import Blueprint
+from flask import Blueprint, render_template
 from auth import get_access_token
 
 playback_bp = Blueprint('playback', __name__)
 
 @playback_bp.route('/')
-def current_state():
-    return """
-    <html>
-        <body>
-            <iframe src="/playback/current_state" width="100%" height="100%" frameborder="0"></iframe>
-        </body>
-    </html>
-    """
+def playback():
+    return render_template('playback/playback.html')
 
 @playback_bp.route('/current_state')
-def get_playback_state():
+def current_state():
     ACCESS_TOKEN = get_access_token()
     
     url = "https://api.spotify.com/v1/me/player/currently-playing?market=US"
@@ -38,44 +32,15 @@ def get_playback_state():
         total_minutes = duration_ms // 60000
         total_seconds = (duration_ms % 60000) // 1000
         time_remaining = (duration_ms - progress_ms) // 1000 + 5 # Refresh after the song ends with a 5-second buffer
-        save_track_csv(artist, track, album, progress_ms, duration_ms)
-        return f"""
-        <html>
-            <head>
-                <meta http-equiv="refresh" content="{time_remaining}">
-            </head>
-            <body>
-            <h1>Currently playing: {track} by {artist}</h1>
-            <p>Album: {album}</p>
-            <p>Progress: {progress_minutes}:{progress_seconds:02d} / {total_minutes}:{total_seconds:02d}</p>
-            </body>
-        </html>
-        """
+        # save_track_csv(artist, track, album, progress_ms, duration_ms)
+        return render_template('playback/current_state_200.html', time_remaining=time_remaining, track=track, artist=artist, album=album, progress_minutes=progress_minutes, progress_seconds=progress_seconds, total_minutes=total_minutes, total_seconds=total_seconds)
     elif response.status_code == 204:
-        return """
-        <html>
-            <head>
-                <meta http-equiv="refresh" content="5">
-            </head>
-            <body>
-            <h1>No music is currently playing.</h1>
-            </body>
-        </html>
-        """
+        return render_template('playback/current_state_204.html')
     elif response.status_code == 429:
         retry_after = int(response.headers.get("Retry-After", 5))
-        return f"""
-        <html>
-            <head>
-            <meta http-equiv="refresh" content="{retry_after}">
-            </head>
-            <body>
-            <h1>Rate limit exceeded. Retrying after {retry_after} seconds.</h1>
-            </body>
-        </html>
-        """
+        return render_template('playback/current_state_429.html', retry_after=retry_after)
     else:
-        return {"code": response.status_code, "message": response.reason}
+        return render_template('playback/current_state_error.html', status_code=response.status_code, message=response.reason)
     
 def save_track_csv(artist, track, album, progress_ms, duration_ms):
     played_at = datetime.now(timezone('US/Eastern'))

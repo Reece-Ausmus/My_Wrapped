c9bb714a511ff3020be859507b162e1c9a98f7c7
Reece Ausmus <reece@ausmusfamily.com>
2024-12-28 12:39:39 -0500

    make default use selenium webdriver to open the server. -c flag to use webbrowser. open server in separate thread

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import os
+import threading
+import time
 import webbrowser
 from flask import Flask
+import requests
 from auth import auth_bp
 from playback import playback_bp
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import signal
+from selenium import webdriver
+import argparse
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -28,9 +34,71 @@ def create_app():
 
     return app
 
+def run_server(app):
+    app.run(port=12398)
+
+def wait_for_server(url):
+    while True:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                break
+        except requests.ConnectionError:
+            pass
+        time.sleep(1)
+
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Run the Flask app.')
+    parser.add_argument('-c', '--use-webbrowser', action='store_true', help='Use webbrowser to open the URL instead of webdriver')
+    args = parser.parse_args()
+
     app = create_app()
-    print("Starting the server... Opening the login page.")
-    webbrowser.open('http://127.0.0.1:12398/auth')
-    app.run(port=12398)
+    url = 'http://127.0.0.1:12398/auth'
+
+    print("Starting the server...")
+
+    # Start the Flask server in a separate thread
+    server_thread = threading.Thread(target=run_server, args=(app,))
+    server_thread.start()
+
+    # Wait for server to be ready
+    wait_for_server(url)
+
+    print("Opening the login page...")
+    if args.use_webbrowser:
+        webbrowser.open(url)
+        def signal_handler(sig, frame):
+            print("\nStopping the server...")
+            os.kill(os.getpid(), signal.SIGTERM)
+        
+        signal.signal(signal.SIGINT, signal_handler)
+        signal.pause()
+    else:
+        driver = webdriver.Chrome()
+        driver.get(url)
+        def check_browser():
+            while True:
+                try:
+                    if not driver.window_handles:
+                        break
+                except:
+                    break
+                time.sleep(1)
+                print("Browser closed by the user...")
+                driver.quit()
+                print("Stopping the server...")
+                os.kill(os.getpid(), signal.SIGTERM)
+
+        browser_thread = threading.Thread(target=check_browser)
+        browser_thread.start()
+        
+        def signal_handler(sig, frame):
+            print("\nClosing the web browser...")
+            driver.quit()
+            print("Stopping the server...")
+            os.kill(os.getpid(), signal.SIGTERM)
+        
+        signal.signal(signal.SIGINT, signal_handler)
+        signal.pause()
